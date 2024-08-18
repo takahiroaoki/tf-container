f8e2095532aaa6902d4bb4f886ef43c77cd8e6b6
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2024-08-18 12:29:12 +0000

    :sparkles: add network resource

--- projects/sample-aws/main.tf
+++ projects/sample-aws/main.tf
@@ -29,14 +29,3 @@ variable "project" {
 variable "environment" {
   type = string
 }
-
-# ---------------------------------------------
-# Resources
-# ---------------------------------------------
-resource "aws_instance" "sample-by-terraform" {
-  ami           = "ami-0091f05e4b8ee6709"
-  instance_type = "t2.micro"
-  tags = {
-    "Name" = "hello"
-  }
-}
