e507126aff0617cf2340eace3ac2829736710b35
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2024-08-20 00:44:18 +0000

    :sparkles: add RDS

--- projects/sample-aws/rds.tf
+++ projects/sample-aws/rds.tf
@@ -49,46 +49,46 @@ resource "random_string" "db_password" {
   special = false
 }
 
-#resource "aws_db_instance" "mysql_standalone" {
-#  engine         = "mysql"
-#  engine_version = "8.0.32"
-#
-#  identifier = "${var.project}-${var.environment}-mysql-standalone"
-#
-#  username = "admin"
-#  password = random_string.db_password.result
-#
-#  instance_class = "db.t4g.medium"
-#
-#  allocated_storage     = 20
-#  max_allocated_storage = 50
-#  storage_type          = "gp2"
-#  storage_encrypted     = false
-#
-#  multi_az               = false
-#  availability_zone      = "ap-northeast-1a"
-#  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
-#  vpc_security_group_ids = [aws_security_group.db_sg.id]
-#  publicly_accessible    = false
-#  port                   = 3306
-#
-#  name                 = var.project
-#  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
-#  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
-#
-#  backup_window              = "04:00-05:00"
-#  backup_retention_period    = 7
-#  maintenance_window         = "Mon:05:00-Mon:08:00"
-#  auto_minor_version_upgrade = false
-#
-#  deletion_protection = false
-#  skip_final_snapshot = true
-#
-#  apply_immediately = true
-#
-#  tags = {
-#    Name    = "${var.project}-${var.environment}-mysql-standalone"
-#    Project = var.project
-#    Env     = var.environment
-#  }
-#}
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.39"
+
+  identifier = "${var.project}-${var.environment}-mysql-standalone"
+
+  username = "admin"
+  password = random_string.db_password.result
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 20
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = var.project
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = false
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone"
+    Project = var.project
+    Env     = var.environment
+  }
+}
